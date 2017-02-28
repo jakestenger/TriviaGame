31c0a71a0653fc6692a749e5336084109b8cfbb9
Jake Stenger <myselfjake@gmail.com>
2017-02-27 17:51:49 -0800

    initial save

---
+++ assets/javascript/app.js
@@ -0,0 +1,87 @@
+var score = 0;
+var questions = {};
+var question_time = 8000;
+
+"What was the registration number of the Enterprise from Star Trek (The Original Series)?"
+"NCC-1701A"
+"NCC-1701D"
+"NX-01"
+"2FAST2TREK"
+
+"Who was the captain of the Enterprise in Star Trek: The Next Generation?"
+"Kirk"
+"Spock"
+"Picard"
+"Archer"
+
+"Which Star Trek \"Captain\" wasn't actually a Captain when their show started?"
+"Kirk"
+"Picard"
+"Sisko"
+"Janeway"
+
+"Which Star Trek enemy was able to convert Captain Picard to their side for two episodes?"
+"Q"
+"Borg"
+"Romulans"
+"Maquis"
+
+"Which long running 90's sitcom had more regulars act as guest starts in Star Trek: The Next Generation than any other show?"
+"Seinfeld"
+"Roseanne"
+"Friends"
+"Cheers"
+
+"Which Star Trek series broke from the tradition of having an entirely orchestral theme song to using a pop song?"
+"Star Trek: The Animated Series"
+"Voyager"
+"Star Trek: The Next Generation"
+"Enterprise"
+
+"Which of these Star Trek races lives the longest?"
+"Human"
+"Vulcan"
+"Q"
+"Ocampa"
+
+"Vulcans as a race have learned over millenia to suppress their emotions and instead base all of their decisions on..."
+"chance"
+"logic"
+"luck"
+"the Vulcan science directorate"
+
+"Some people critisize Star Trek aliens as looking like \"just humans with stuff glued to their faces\". Which alien race looks like it has the most stuff glued to its face?"
+"Vulcans"
+"Bajorans"
+"Klingons (23rd century)"
+"Klingons (24th century)"
+
+"The hand-held devices that Starfleet personnel use to scan things in their immediate viscinity are called..."
+"tricorders"
+"hand-scanners"
+"scantrons"
+"iScanners"
+
+"Which of these Star Trek characters did NOT appear as a recurring character on more than one Star Trek series?"
+"Data"
+"Chief O'Brien"
+"Q"
+"Worf"
+
+"Data, like a character in The Wizard of Oz, was an android who lacked one key element of humanity. He lacked..."
+"Sexuality"
+"Emotions"
+"A brain"
+"Courage"
+
+"Data, the android from Star Trek: The Next Generation, had a pet. The pet was..."
+"A cat named Spot"
+"An android dog named Quark"
+"A targarian eel named Data Jr."
+"A smaller android version of himself named Datum"
+
+"Ferengi are an alien race obsessed with..."
+"Wealth"
+"Morality"
+"Scientific knowledge"
+"Looking pretty"
