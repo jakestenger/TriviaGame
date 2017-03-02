8cfcaf6c6ac0268994363df51ec233ff6e76adf0
jakestenger <myselfjake@gmail.com>
2017-03-02 06:57:44 -0800

    save

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,107 +1,60 @@
 var score = 0;
-var questions = {};
 var question_time = 8000;
+var questions = {
+	q: "What was the registration number of the Enterprise from Star Trek (The Original Series)?",
+	a: ["NCC-1701A", "NCC-1701D", "NX-01", "2FAST2TREK"],
 
-"What was the registration number of the Enterprise from Star Trek (The Original Series)?"
-"NCC-1701A"
-"NCC-1701D"
-"NX-01"
-"2FAST2TREK"
+	q: "Who was the captain of the Enterprise in Star Trek: The Next Generation?",
+	a: ["Kirk", "Spock", "Picard", "Archer", 
 
-"Who was the captain of the Enterprise in Star Trek: The Next Generation?"
-"Kirk"
-"Spock"
-"Picard"
-"Archer"
+	q: "Which Star Trek \"Captain\" wasn't actually a Captain when their show started?",
+	a: ["Kirk", "Picard", "Sisko", "Janeway"
 
-"Which Star Trek \"Captain\" wasn't actually a Captain when their show started?"
-"Kirk"
-"Picard"
-"Sisko"
-"Janeway"
+	q: "Which Star Trek enemy was able to convert Captain Picard to their side for two episodes?",
+	a: ["Q", "Borg", "Romulans", "Maquis"
 
-"Which Star Trek enemy was able to convert Captain Picard to their side for two episodes?"
-"Q"
-"Borg"
-"Romulans"
-"Maquis"
+	q: "Which long running 90's sitcom had more regulars act as guest starts in Star Trek: The Next Generation than any other show?",
+	a: ["Seinfeld", "Roseanne", "Friends", "Cheers"
 
-"Which long running 90's sitcom had more regulars act as guest starts in Star Trek: The Next Generation than any other show?"
-"Seinfeld"
-"Roseanne"
-"Friends"
-"Cheers"
+	q: "Which Star Trek series broke from the tradition of having an entirely orchestral theme song to using a pop song?",
+	a: ["Star Trek: The Animated Series", "Voyager", "Star Trek: The Next Generation", "Enterprise"
 
-"Which Star Trek series broke from the tradition of having an entirely orchestral theme song to using a pop song?"
-"Star Trek: The Animated Series"
-"Voyager"
-"Star Trek: The Next Generation"
-"Enterprise"
+	q: "Which of these Star Trek races lives the longest?",
+	a: ["Human", "Vulcan", "Q", "Ocampa"
 
-"Which of these Star Trek races lives the longest?"
-"Human"
-"Vulcan"
-"Q"
-"Ocampa"
+	q: "Vulcans as a race have learned over millenia to suppress their emotions and instead base all of their decisions on...",
+	a: ["chance", "logic", "luck", "the Vulcan science directorate"
 
-"Vulcans as a race have learned over millenia to suppress their emotions and instead base all of their decisions on..."
-"chance"
-"logic"
-"luck"
-"the Vulcan science directorate"
+	q: "Some people critisize Star Trek aliens as looking like \"just humans with stuff glued to their faces\". Which alien race looks like it has the most stuff glued to its face?",
+	a: ["Vulcans", "Bajorans", "Klingons (23rd century)", "Klingons (24th century)"
 
-"Some people critisize Star Trek aliens as looking like \"just humans with stuff glued to their faces\". Which alien race looks like it has the most stuff glued to its face?"
-"Vulcans"
-"Bajorans"
-"Klingons (23rd century)"
-"Klingons (24th century)"
+	q: "The hand-held devices that Starfleet personnel use to scan things in their immediate viscinity are called...",
+	a: ["tricorders", "hand-scanners", "scantrons", "iScanners"
 
-"The hand-held devices that Starfleet personnel use to scan things in their immediate viscinity are called..."
-"tricorders"
-"hand-scanners"
-"scantrons"
-"iScanners"
+	q: "Which of these Star Trek characters did NOT appear as a recurring character on more than one Star Trek series?",
+	a: ["Data", "Chief O'Brien", "Q", "Worf"
 
-"Which of these Star Trek characters did NOT appear as a recurring character on more than one Star Trek series?"
-"Data"
-"Chief O'Brien"
-"Q"
-"Worf"
+	q: "Data, like a character in The Wizard of Oz, was an android who lacked one key element of humanity. He lacked...",
+	a: ["Sexuality", "Emotions", "A brain", "Courage"
 
-"Data, like a character in The Wizard of Oz, was an android who lacked one key element of humanity. He lacked..."
-"Sexuality"
-"Emotions"
-"A brain"
-"Courage"
+	q: "Data, the android from Star Trek: The Next Generation, had a pet. The pet was...",
+	a: ["A cat named Spot", "An android dog named Quark", "A targarian eel named Data Jr.", "A smaller android version of himself named Datum"
 
-"Data, the android from Star Trek: The Next Generation, had a pet. The pet was..."
-"A cat named Spot"
-"An android dog named Quark"
-"A targarian eel named Data Jr."
-"A smaller android version of himself named Datum"
+	q: "Ferengi are an alien race obsessed with...",
+	a: ["Wealth", "Morality", "Scientific knowledge", "Looking pretty"
 
-"Ferengi are an alien race obsessed with..."
-"Wealth"
-"Morality"
-"Scientific knowledge"
-"Looking pretty"
+	q: "The crew of Voyager got lost 70,000 years from home in which quadrant of the Milky Way?",
+	a: ["Alpha", "Beta", "Delta", "Gamma"
 
-"The crew of Voyager got lost 70,000 years from home in which quadrant of the Milky Way?"
-"Alpha"
-"Beta"
-"Delta"
-"Gamma"
+	q: "Which Star Trek character's actress later went on to play a major character in Netflix's Orange Is The New Black?",
+	a: ["Major Kira Nerys", "Captain Janeway", "7 of 9", "Deanna Troi"
 
-"Which Star Trek character's actress later went on to play a major character in Netflix's Orange Is The New Black?"
-"Major Kira Nerys"
-"Captain Janeway"
-"7 of 9"
-"Deanna Troi"
+	q: "Starfleet is the military branch of which governmental organization?",
+	a: ["Federation of Planets", "United Solar Systems", "League of Spacefaring Civilizations", "The Republic"
 
-"Starfleet is the military branch of which governmental organization?"
-"Federation of Planets"
-"United Solar Systems"
-"League of Spacefaring Civilizations"
-"The Republic"
+	q: "Captain Archer, from Enterprise, is an avid watcher of what sport?",
+	a: ["Baseball", "Quidditch", "Water Polo", "Klingon Dog Fighting"
 
-
+	q: "Which character from Enterprise is often referred to, but never appears on screen?",
+	a: ["Ambassador Soval", "Chef", "The Captain's dog, Porthos", "Commander Trip Tucker"
+}
